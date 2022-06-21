ebb48380f0ce04ffd4b0ea840fca61b6fbd29bb7
Kasi Acosta <cacosta@megared.int>
2022-06-20 21:50:37 -0300

    desafio navBar

---
+++ src/Componentes/NavBar/NavBar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Logo from '../../assets/logo.jpeg'
+import './NavBar.css'
+const NavBar = () => {
+    return(
+        <header style={styles.contenedor}>
+        <img style={styles.imagen} src={Logo} alt="logo" />
+        <h1>Buho Indumentaria</h1>
+        <nav style={styles.navStyle}>
+            <a style={styles.anchor} href="">Inicio</a>
+            <a style={styles.anchor} href="">Tienda</a>
+            <a style={styles.anchor} href="">Nosotros</a>
+        </nav>
+        
+        </header>
+    )
+}
+
+export default NavBar
+
+const styles = {
+
+contenedor:{
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    fontFamily: 'Koulen', 
+    backgroundColor: 'black',
+    color: '#f185d1 ',
+},
+navStyle:{
+    display: 'flex',
+    justifyContent: 'space-between',
+},
+
+anchor:{
+    paddingLeft: 5,
+    textDecoration: 'none',
+},
+imagen:{
+    width: '10%',
+}
+}
