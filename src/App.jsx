5dbaaddb5d163db7515bde50c7ef67fbe9c01292
Kasi Acosta <cacosta@megared.int>
2022-07-06 13:26:58 -0300

    desafio Detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import NavBar from './Componentes/NavBar/NavBar.js';
 import './App.css';
-import ItemListContainer from './Componentes/ItemListContainer.jsx';
+import ItemListContainer from './Componentes/ItemListContainer';
+import ItemDetailContainer from './Componentes/ItemDetailContainer';
 
 const App = () => {
   return (
     <>
     <NavBar />,
     <ItemListContainer name = 'Buho Indumentaria' />
+    <ItemDetailContainer />
     </>
   );
 }
--- src/Componentes/ItemListContainer.jsx
+++ src/Componentes/ItemListContainer.jsx
@@ -2,62 +2,35 @@ import React, { useState, useEffect } from "react";
 import ItemCount from './ItemCount';
 import Itemlist from "./ItemList";
 
-
-const listadoProductos = [
-    {id:0, title:'remera', description:'a rayas', price: 1200, pricture: 'image1'},
-    {id:1, title:'camisa', description:'roja', price: 3000, pricture: 'image2'},
-    {id:2, title:'pantalon', description:'verde', price: 2500, pricture: 'image3'},
-    {id:3, title:'bolso', description:'playero', price: 7500, pricture: 'image4'},
-    {id:4, title:'gorro', description:'vaquero', price: 2800, pricture: 'image5'},
-    {id:5, title:'short', description:'rosa', price: 9200, pricture: 'image6'},
-
-]
-
-const promes = new Promise ((res,rej) => {
-    setTimeout(() => {
-        res(listadoProductos)
-    }, 4000);
-})
-
 const ItemListContainer =  (prop) => {
     const { name } = prop;
     const [productos, setProductos] = useState([]);
-
-    const [loading, setLoadin] = useState (false);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState (true);
     
     useEffect(() =>{
-        setLoadin(true);
-        promes.then((data)=>{
-            setLoadin(false);
-            console.log(data)
-            setProductos(data)
-        }).catch((data) =>{
-            console.log('error')
-        })
+      const getProducts = async () =>{
+      try {const response = await fetch("../InfoProducts/infoProducts.json");
+        const data = await response.json();
+        setProductos(data);}
+        catch(err) {
+          console.log(err);
+          setError(true);
+        }
+        finally{
+          setLoading(false);
+        }
+      }
+      getProducts();
     },[]);
 
-    if (loading){
-        return(
-            <div class="preloader-wrapper active">
-            <div class="spinner-layer spinner-red-only">
-              <div class="circle-clipper left">
-                <div class="circle"></div>
-              </div><div class="gap-patch">
-                <div class="circle"></div>
-              </div><div class="circle-clipper right">
-                <div class="circle"></div>
-              </div>
-            </div>
-          </div>
-        );
-    }
-
+   
     return (
         <>
         <h2 class="z-depth-2" style={styles.subTitulo}>
           En {name} vas a encontrar todo lo que buscas
         </h2>
-
+        {loading ? <p>Loading...</p> : error ? <p>Error...</p> : 'Todo bien'}
         <Itemlist productos={productos}/>
         <ItemCount stock = {8} initial = {1}/>
         </>
