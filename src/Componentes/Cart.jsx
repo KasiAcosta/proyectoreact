ebb19294b6bbae07823e115ed73aa7557ab61653
Kasi Acosta <cacosta@megared.int>
2022-07-20 20:40:10 -0300

    desafio cart view

--- src/Componentes/Cart.jsx
+++ src/Componentes/Cart.jsx
@@ -1,12 +1,18 @@
 import React, { useContext } from "react";
 import { context } from '../context/CartContext';
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-    const { compra } = useContext(context);
+    const { compra, removeItem, clear } = useContext(context);
 
     return(
-        <>{compra.length === 0 ? <h1> Aun no añadiste productos al Carrito </h1>
-        : <>{compra.map(item => <h1 key={item.id}>{item.title}</h1>)}</>}</>
+        <>
+        {compra.length === 0 ? <div><h1> Aun no añadiste productos al Carrito </h1> <Link className="waves-effect waves-light btn pink" to="/">Ir a productos</Link>
+        </div>
+        : <>{compra.map(item => <h1 key={item.id}>{item.title}</h1>)}</>}
+        <button onClick={removeItem}> eliminar</button>
+        <button onClick={clear}> limpiar</button>
+        </>
     )
 }
 
